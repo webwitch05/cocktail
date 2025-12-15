1888b7f2849f3e58da3db4ac04e00a175db5080b
webwitch05 <webwitch05@gmail.com>
2025-12-15 23:15:50 +0800

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import './App.css'
 import gsap from 'gsap';
 
+import Navbar from './components/Navbar';
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
 
   return (
-    <div className='flex-center h-[100vh]'>
-      <h1 className='text-3xl'>Hello, GSAP!</h1>
-    </div>
+    <main>
+      <Navbar/>
+    </main>
   )
 }
 
